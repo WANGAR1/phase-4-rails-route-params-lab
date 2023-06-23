723954d9c47b5d3324e9a7e896c4c48cd7fc8523
Wangari Samantha <samanthanyams5@gmail.com>
2023-06-23 03:57:44 +0300

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,18 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end  
+
+  def index
+    if params[:query].present?
+      students = Student.where("first_name LIKE :query OR last_name LIKE :query", query: "%#{params[:query]}%")
+    else
+      students = Student.all
+    end
+    render json: students
+  end  
+
 end
